4a36467d4e6d0fdcc99a98c132a3aae940939d60
Cyriel Bastin <cyriel.bastin@zoho.com>
2020-09-23 22:48:12 +0200

    setup Spring Boot

---
+++ src/main/java/com/project/blog__video_game/BlogVideoGameApplication.java
@@ -0,0 +1,13 @@
+package com.project.blog__video_game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogVideoGameApplication
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(BlogVideoGameApplication.class, args);
+    }
+}
